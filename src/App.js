11dd7a1cd23dc2cbbc3b023d9abb43212e56f477
nini070 <nirjala.prajapati07@gmail.com>
2023-12-30 10:29:07 +0545

    first

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
 import './App.css';
-
+// import ClassParent from './Class.jsx';
+// import Comp from './component.jsx';
+// import DisplayDataComp from './cw.jsx';
+// import Main from './Main.jsx';
+import Header from './Header.jsx';
+import Sidenav from './Sidenav.jsx';
+import Footer from './Footer.jsx';
+import MainContent from './Maincontent.jsx';
+import './assignment.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* <Main/> */}
+      {/* <ClassParent/> */}
+      {/* <Comp/> */}
+      {/* <DisplayDataComp/> */}
+      <Header title="Header"/>
+      <div className= "Mid">
+        <Sidenav title="Side Nav"/>
+        <MainContent title="Main Content"/>
+      </div>
+      <Footer title="Footer"/>
     </div>
   );
 }
-
 export default App;
